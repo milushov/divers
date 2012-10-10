7ea643c042618b4621d05df96af62f61315f96c7
Roman Milushov <github@milushov.ru>
2012-10-10 06:04:48 +0400

    add util for extending objects

--- javascripts/utils.js
+++ javascripts/utils.js
@@ -16,3 +16,20 @@ window.requestAnimFrame = (function(){
             window.setTimeout(callback, 1000 / 60);
           };
 })();
+
+var __hasProp = {}.hasOwnProperty;
+var extend = function(child, parent) {
+  for (var key in parent) {
+    if (__hasProp.call(parent, key)) child[key] = parent[key];
+  }
+
+  function ctor() {
+    this.constructor = child;
+  }
+
+  ctor.prototype = parent.prototype;
+  child.prototype = new ctor();
+  child.__super__ = parent.prototype;
+
+  return child;
+};
