ce65912ead714f5b49aeaf01ca31ae10aea17413
Roman Milushov <github@milushov.ru>
2012-10-21 11:45:33 +0400

    options

--- javascripts/background.js
+++ javascripts/background.js
@@ -1,5 +1,18 @@
-function Background() {
+function Background(debug) {
+  this.config = {
+    speed: {
+    },
+
+    objects: {
+    },
+
+    options: {
+    }
+  };
+
   this.init = function() {
     console.log('bg init');
+    this.canvas = document.getElementById('background');
+    this.ctx = this.canvas.getContext('2d');
   };
 }
