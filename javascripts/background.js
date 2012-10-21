08caebe61297f39392a85bc6c1848328148c7a98
Roman Milushov <github@milushov.ru>
2012-10-21 17:17:06 +0400

    function for drawing rounded rectangles

--- javascripts/background.js
+++ javascripts/background.js
@@ -32,7 +32,7 @@ function Background(debug) {
   };
 
   this.draw = function() {
-    //this.drawBackground();
+    this.drawBackground();
     //this.drawSea();
     for (var i = 0; i < this.fishes.length; ++i) {
       this.fishes[i].draw();
@@ -42,4 +42,15 @@ function Background(debug) {
       this.clouds[i].draw();
     }
   };
+
+  this.drawBackground = function() {
+    var w = this.canvas.width, h = this.canvas.height;
+    var gradient = this.ctx.createLinearGradient(100,90,100,200);
+    gradient.addColorStop(0,'rgb(49, 104, 224)');
+    gradient.addColorStop(.5,'rgb(49, 72, 224)');
+    gradient.addColorStop(1,'rgb(49, 53, 224)');
+    this.ctx.fillStyle = gradient;
+
+    this.ctx.roundRect(0, 0, w, h, 20, true);
+  }
 }
--- javascripts/utils.js
+++ javascripts/utils.js
@@ -111,3 +111,36 @@ function isDebug() {
     return true;
   }
 }
+
+if(!CanvasRenderingContext2D.prototype.roundRect) {
+  CanvasRenderingContext2D.prototype.roundRect = 
+    function(x, y, width, height, radius, fill, stroke) {
+
+    if (typeof stroke == "undefined" ) {
+      stroke = true;
+    }
+    if (typeof radius === "undefined") {
+      radius = 5;
+    }
+
+    this.beginPath();
+    this.moveTo(x + radius, y);
+    this.lineTo(x + width - radius, y);
+    this.quadraticCurveTo(x + width, y, x + width, y + radius);
+    this.lineTo(x + width, y + height - radius);
+    this.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+    this.lineTo(x + radius, y + height);
+    this.quadraticCurveTo(x, y + height, x, y + height - radius);
+    this.lineTo(x, y + radius);
+    this.quadraticCurveTo(x, y, x + radius, y);
+    this.closePath();
+
+    if (stroke) {
+      this.stroke();
+    }
+
+    if (fill) {
+      this.fill();
+    }
+  }
+}
